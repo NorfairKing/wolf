885966c156be620f70d74e8f087b8f670faecb00
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-06-22 19:00:39 +0100

    Note doesn't work without init

--- wolf/src/Wolf/Note.hs
+++ wolf/src/Wolf/Note.hs
@@ -10,6 +10,7 @@ import Data.Time
 import Wolf.Editor
 import Wolf.Git
 import Wolf.Index
+import Wolf.Init
 import Wolf.JSONUtils
 import Wolf.NoteIndex
 import Wolf.OptParse.Types
@@ -17,36 +18,39 @@ import Wolf.Path
 import Wolf.Types
 
 note :: (MonadIO m, MonadReader Settings m) => String -> m ()
-note person = do
-    origIndex <- getIndex
-    (personUuid, index) <- lookupOrCreateNewPerson person origIndex
-    origNoteIndex <- getNoteIndex personUuid
-    (noteUuid, noteIndex) <- createNewNote personUuid origNoteIndex
-    tnf <- tmpPersonNoteFile personUuid noteUuid
-    editingResult <- startEditorOn tnf
-    case editingResult of
-        EditingFailure reason ->
-            liftIO $
-            putStrLn $
-            unwords
-                [ "ERROR: failed to edit the note file:"
-                , show reason
-                , ",not saving."
-                ]
-        EditingSuccess -> do
-            now <- liftIO getCurrentTime
-            contents <- liftIO $ T.readFile $ toFilePath tnf
-            let personNote =
-                    PersonNote
-                    {personNoteContents = contents, personNoteTimestamp = now}
-            nf <- personNoteFile personUuid noteUuid
-            writeJSON nf personNote
-            putIndex index
-            putNoteIndex personUuid noteIndex
-            makeGitCommit $
+note person =
+    withInitCheck $ do
+        origIndex <- getIndex
+        (personUuid, index) <- lookupOrCreateNewPerson person origIndex
+        origNoteIndex <- getNoteIndex personUuid
+        (noteUuid, noteIndex) <- createNewNote personUuid origNoteIndex
+        tnf <- tmpPersonNoteFile personUuid noteUuid
+        editingResult <- startEditorOn tnf
+        case editingResult of
+            EditingFailure reason ->
+                liftIO $
+                putStrLn $
                 unwords
-                    [ "Added note on"
-                    , person
-                    , "with uuid"
-                    , personNoteUuidString noteUuid
+                    [ "ERROR: failed to edit the note file:"
+                    , show reason
+                    , ",not saving."
                     ]
+            EditingSuccess -> do
+                now <- liftIO getCurrentTime
+                contents <- liftIO $ T.readFile $ toFilePath tnf
+                let personNote =
+                        PersonNote
+                        { personNoteContents = contents
+                        , personNoteTimestamp = now
+                        }
+                nf <- personNoteFile personUuid noteUuid
+                writeJSON nf personNote
+                putIndex index
+                putNoteIndex personUuid noteIndex
+                makeGitCommit $
+                    unwords
+                        [ "Added note on"
+                        , person
+                        , "with uuid"
+                        , personNoteUuidString noteUuid
+                        ]
