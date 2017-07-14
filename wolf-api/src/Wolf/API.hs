0028e6febcda8a2dce4bd4f7e394457e52877f29
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-07-14 07:34:55 +0200

    started on wolf-api

---
+++ wolf-api/src/Wolf/API.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Wolf.API where
+
+import Servant.API
+
+import Wolf.Types
+
+type WolfAPI = PersonAPI
+
+type PersonAPI = PostNewPerson :<|> GetPersonEntry
+
+type PostNewPerson
+     = "person" :> "new" :> ReqBody '[ JSON] PersonEntry :> Post '[ JSON] PersonUuid
+
+type GetPersonEntry
+     = "person" :> Capture "person-uuid" PersonUuid :> Get '[ JSON] [PersonEntry]
