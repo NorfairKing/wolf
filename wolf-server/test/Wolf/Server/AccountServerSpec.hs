f81b98b8b219c1611fa250ff16a2620b7b18e058
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-08-25 02:12:07 +0200

    another test for registration

--- wolf-server/test/Wolf/Server/AccountServerSpec.hs
+++ wolf-server/test/Wolf/Server/AccountServerSpec.hs
@@ -22,19 +22,36 @@ import Wolf.Server.TestUtils
 spec :: Spec
 spec =
     withWolfServer $
-    describe "postRegister" $
-    it "does not crash" $ \cenv ->
-        forAll genValid $ \register -> do
-            errOrUuid <- runClient cenv $ clientPostRegister register
-            case errOrUuid of
-                Left err ->
-                    let snf =
-                            expectationFailure $
-                            "Should not fail with error: " <> show err
-                    in case err of
-                           FailureResponse {} ->
-                               if statusCode (responseStatus err) == 409
-                                   then pure ()
-                                   else snf
-                           _ -> snf
-                Right uuid -> uuid `shouldSatisfy` isValid
+    describe "postRegister" $ do
+        it "does not crash" $ \cenv ->
+            forAll genValid $ \register -> do
+                errOrUuid <- runClient cenv $ clientPostRegister register
+                case errOrUuid of
+                    Left err ->
+                        let snf =
+                                expectationFailure $
+                                "Should not fail with error: " <> show err
+                        in case err of
+                               FailureResponse {} ->
+                                   if statusCode (responseStatus err) == 409
+                                       then pure ()
+                                       else snf
+                               _ -> snf
+                    Right uuid -> uuid `shouldSatisfy` isValid
+        it "returns a 409 error if the username already exists" $ \cenv ->
+            forAll genValid $ \register ->
+                withValidNewUser cenv $ \_ -> do
+                    errOrUuid <- runClient cenv $ clientPostRegister register
+                    case errOrUuid of
+                        Left err ->
+                            let snf =
+                                    expectationFailure $
+                                    "Should not fail with error: " <> show err
+                            in case err of
+                                   FailureResponse {} ->
+                                       if statusCode (responseStatus err) == 409
+                                           then pure ()
+                                           else snf
+                                   _ -> snf
+                        Right _ ->
+                            expectationFailure "Should not have succeeded."
