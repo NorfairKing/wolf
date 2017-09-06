69ecad01a893d0a25a99c4b64eac77321af80a0f
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-09-06 08:51:25 +0200

    Basic suggestion manipulation

--- wolf-data/src/Wolf/Data/Suggestion.hs
+++ wolf-data/src/Wolf/Data/Suggestion.hs
@@ -1,5 +1,35 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Wolf.Data.Suggestion
     ( Suggestion(..)
+    , readPersonEntrySuggestions
+    , writePersonEntrySuggestions
     ) where
 
+import Import
+
+import Wolf.Data.Entry.Types
+import Wolf.Data.JSONUtils
+import Wolf.Data.Path
+import Wolf.Data.Types
+
 import Wolf.Data.Suggestion.Types
+
+suggestionsDir :: MonadReader DataSettings m => m (Path Abs Dir)
+suggestionsDir = (</> $(mkRelDir "suggestions")) <$> wolfDir
+
+entrySuggestionsFile :: MonadReader DataSettings m => m (Path Abs File)
+entrySuggestionsFile = (</> $(mkRelFile "entry")) <$> suggestionsDir
+
+readPersonEntrySuggestions ::
+       (MonadIO m, MonadReader DataSettings m) => m [Suggestion PersonEntry]
+readPersonEntrySuggestions = entrySuggestionsFile >>= readJSONWithDefault []
+
+writePersonEntrySuggestions ::
+       (MonadIO m, MonadReader DataSettings m)
+    => [Suggestion PersonEntry]
+    -> m ()
+writePersonEntrySuggestions ess = do
+    f <- entrySuggestionsFile
+    writeJSON f ess
---
+++ wolf-test-utils/test/Wolf/Data/SuggestionSpec.hs
@@ -0,0 +1,24 @@
+module Wolf.Data.SuggestionSpec
+    ( spec
+    ) where
+
+import TestImport
+
+import Wolf.Data.Suggestion
+import Wolf.Data.TestUtils
+
+import Wolf.Data.Entry.Types.Gen ()
+import Wolf.Data.Suggestion.Types.Gen ()
+
+spec :: Spec
+spec =
+    withDataSetsGen $
+    describe "readPersonEntrySuggestions" $
+    it "reads the suggestions that were just written" $ \gen ->
+        forAll gen $ \sets ->
+            forAll genValid $ \sugs -> do
+                sugs' <-
+                    flip runReaderT sets $ do
+                        writePersonEntrySuggestions sugs
+                        readPersonEntrySuggestions
+                sugs' `shouldBe` sugs
