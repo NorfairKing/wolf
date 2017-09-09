4155168a4aa44ba1c61e19023d16c8f19f63bb2a
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-09-09 23:24:08 +0200

    little suggestion refactor

---
+++ wolf-cli/src/Wolf/Cli/Command/Suggestion/List.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Wolf.Cli.Command.Suggestion.List where
+
+import Import
+
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+
+import System.Console.ANSI as ANSI
+
+import Wolf.Data
+
+import Wolf.Cli.Command.Entry.Internal (tmpEntryFileContents) -- TODO move this somewhere else
+import Wolf.Cli.OptParse.Types
+import Wolf.Cli.Report
+import Wolf.Cli.Utils
+
+listSuggestions :: (MonadIO m, MonadReader Settings m) =>  m ()
+listSuggestions = do
+    sugs <- runData readPersonEntrySuggestions
+    liftIO $
+        mapM_
+            (putStr . renderReport . renderSuggestion renderEntrySuggestion)
+            sugs
+
+renderSuggestion :: (a -> Report) -> Suggestion a -> Report
+renderSuggestion func Suggestion {..} =
+    unlinesReport
+        [ colored [SetColor Foreground Dull Blue] $
+          "Suggestor: " ++ T.unpack suggestionSuggestor
+        , colored [SetColor Foreground Dull Yellow] $
+          "Reason: " ++ T.unpack suggestionReason
+        , func suggestionData
+        ]
+
+renderEntrySuggestion :: EntrySuggestion -> Report
+renderEntrySuggestion EntrySuggestion {..} =
+    case TE.decodeUtf8' $ tmpEntryFileContents entrySuggestionEntry of
+        Left _ ->
+            colored [SetColor Foreground Dull Red] "Failed to decode UTF8 Text"
+        Right t -> stringReport $ T.unpack t
