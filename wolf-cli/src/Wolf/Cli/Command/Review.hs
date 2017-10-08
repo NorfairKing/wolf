9cae67d743d4ba2f74304cbaa58920a8835d9c74
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-10-08 10:19:51 +0200

    refactored review report

--- wolf-cli/src/Wolf/Cli/Command/Review.hs
+++ wolf-cli/src/Wolf/Cli/Command/Review.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Wolf.Cli.Command.Review where
@@ -6,6 +8,7 @@ module Wolf.Cli.Command.Review where
 import Import
 
 import qualified Data.Map as M
+import qualified Data.Set as S
 import qualified Data.Text as T
 import Data.Time
 import System.Console.ANSI as ANSI
@@ -20,27 +23,47 @@ review :: (MonadIO m, MonadReader Settings m) => m ()
 review =
     runData $
     withInitCheck_ $ do
-        index <- getIndexWithDefault
-        let tups =
-                nubBy (\t1 t2 -> snd t1 == snd t2) $ M.toList $ indexMap index
-        noteTups <-
-            fmap concat $
-            forM tups $ \(nn, personUuid) -> do
-                notes <- getPersonNotes personUuid
-                pure $ map ((,) nn) notes
-        let entries = sortOn (noteTimestamp . snd) noteTups
-        now <- liftIO getCurrentTime
-        let report =
-                mconcat $
-                flip map entries $ \(nickName, personNote) ->
-                    unlinesReport
-                        [ colored [SetColor Foreground Dull Blue] $
-                          T.unwords
-                              [ aliasText nickName <> ","
-                              , formatMomentNicely
-                                    now
-                                    (noteTimestamp personNote)
-                              ]
-                        , textReport $ noteContents personNote
-                        ]
-        liftIO $ putReport report
+        rr <- makeReviewReport
+        liftIO $ putReport $ reviewReportReport rr
+
+makeReviewReport :: (MonadIO m, MonadReader DataSettings m) => m ReviewReport
+makeReviewReport = do
+    ns <- getNotes
+    ix <- getIndexWithDefault
+    now <- liftIO getCurrentTime
+    pure $ reviewReport now ix ns
+
+reviewReport :: UTCTime -> Index -> [Note] -> ReviewReport
+reviewReport now ix ns =
+    let noteTups =
+            M.fromList $
+            flip map ns $ \n@Note {..} ->
+                let as =
+                        flip map (S.toList noteRelevantPeople) $ \uuid ->
+                            fromMaybe
+                                (personUuidText uuid)
+                                (aliasText <$>
+                                 reverseIndexLookupSingleAlias uuid ix)
+                in (n, as)
+    in ReviewReport {reviewReportTimestamp = now, reviewReportNotes = noteTups}
+
+data ReviewReport = ReviewReport
+    { reviewReportTimestamp :: UTCTime
+    , reviewReportNotes :: Map Note [Text]
+    } deriving (Show, Eq, Generic)
+
+instance Validity ReviewReport
+
+reviewReportReport :: ReviewReport -> Report
+reviewReportReport ReviewReport {..} =
+    let entries = sortOn (noteTimestamp . fst) $ M.toList reviewReportNotes
+    in mconcat $
+       flip map entries $ \(Note {..}, names) ->
+           unlinesReport
+               [ colored [SetColor Foreground Dull Blue] $
+                 T.unwords
+                     [ T.intercalate ", " names <> ","
+                     , formatMomentNicely reviewReportTimestamp noteTimestamp
+                     ]
+               , textReport noteContents
+               ]
