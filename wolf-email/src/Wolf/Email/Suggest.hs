57c7badf8dd00846fdee93fa576a1704659fcfcd
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-05-13 18:48:41 +0200

    tried t oget started on email

--- wolf-email/src/Wolf/Email/Suggest.hs
+++ wolf-email/src/Wolf/Email/Suggest.hs
@@ -8,13 +8,19 @@ module Wolf.Email.Suggest
 
 import Import
 
+import qualified Data.ByteString as SB
+import Data.List (find)
 import qualified Data.Text as T
-import qualified Data.Text.Encoding as TE
-import qualified Data.Text.IO as TIO
+import qualified Data.Text.IO as T
+
+import System.Exit
 
 import Control.Monad.Reader
 
-import Text.Email.Validate as EmailAddress (toByteString)
+import qualified Codec.MIME.Parse as Email
+import qualified Codec.MIME.Type as Email
+import qualified Network.Mail.Parse.Parsers.HeaderFields as Email
+import Network.Mail.Parse.Types (EmailAddress(..))
 
 import Wolf.Data
 import Wolf.Data.Baked (EmailAddressWithPurpose(..), fromEntry)
@@ -23,4 +29,22 @@ import Wolf.Email.OptParse
 import Wolf.Email.Suggest.Types
 
 wolfEmailSuggest :: (MonadIO m, MonadReader Settings m) => m ()
-wolfEmailSuggest = pure ()
+wolfEmailSuggest = do
+    contents <- liftIO T.getContents
+    let message = Email.parseMIMEMessage contents
+    -- liftIO $ print message
+    let headers = Email.mime_val_headers message
+    -- liftIO $ mapM_ print headers
+    let personParams =
+            catMaybes $
+            map
+                (\hn -> find ((== hn) . Email.paramName) headers)
+                ["from", "to", "cc"]
+    liftIO $ mapM_ print personParams
+    let addresses =
+            fmap concat $
+            forM personParams $ Email.parseEmailAddressList . Email.paramValue
+    liftIO $
+        case addresses of
+            Left err -> die $ T.unpack err
+            Right as -> mapM_ print as
